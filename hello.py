041b289a8b0e5934a171b50dc2c9dc22da318787
Raghav Sharma <sharmaraghav260@gmail.com>
2017-12-04 13:00:23 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/name/<name>')
+def hello_world(name):
+    return render_template("hello.html", name="raghu")
 
 
 if __name__ == "__main__":
